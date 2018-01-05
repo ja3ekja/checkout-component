d3b7df56fcb140771e63240da586ba95a51882fe
ja3ekja <jacekfeliksiak@gmail.com>
2018-01-06 01:54:50 +1100

    Phase3 (#3)
    * add docker-compose with
    
    * Test docker-compose (#2)
    
    * docker-compose working for sql and backend as well
    
    * baked my own image

--- src/main/java/com/pragmaticcoders/checkoutcomponent/CheckoutComponentApplication.java
+++ src/main/java/com/pragmaticcoders/checkoutcomponent/CheckoutComponentApplication.java
@@ -3,10 +3,20 @@ package com.pragmaticcoders.checkoutcomponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class CheckoutComponentApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         SpringApplication.run(CheckoutComponentApplication.class, args);
+
+        String dbURL = "jdbc:sqlserver://db:1433;user=sa;password=RybaPila1!PiesJadl";
+        Connection conn = DriverManager.getConnection(dbURL);
+        if (conn != null) {
+            System.out.println("Connected");
+        }
     }
 }
