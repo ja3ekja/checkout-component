0f315e1b280531525670696b45934c2526462f8c
Jacek Feliksiak <jacek.feliksiak@myob.com>
2018-01-07 13:54:13 +1100

    mock payment service

---
+++ src/main/java/com/pragmaticcoders/checkoutcomponent/services/PaymentServiceMock.java
@@ -0,0 +1,12 @@
+package com.pragmaticcoders.checkoutcomponent.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceMock {
+
+    public void payForReceiptMock(){
+
+    }
+
+}
